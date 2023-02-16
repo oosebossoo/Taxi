47c48d32446d22d078d5410ddbaffdd2fe26b431
oosebossoo <sebek.kasprzak.sk@gmail.com>
2023-02-16 19:39:34 +0100

    time counter added

--- Taxi/Core/UI.cs
+++ Taxi/Core/UI.cs
@@ -88,7 +88,7 @@ namespace Taxi
 
         public void summary(string[] data)
         {
-            Console.WriteLine($"Zamówiono: {data[0]}, szacowany czas oczekiwania to: {data[1]}");
+            Console.WriteLine($"Zamówiono: {data[0]}, szacowany czas oczekiwania to: {data[1]}minut");
             Console.ReadKey();
         }
     }
